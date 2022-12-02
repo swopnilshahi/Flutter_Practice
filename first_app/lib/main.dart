877fac67c5063890cf678818eb790084f53500d0
Swopnil <swopnil@divyaltech.com>
2022-12-02 15:30:37 +0545

    splitting app and updating data

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -14,47 +14,43 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  void ansewerQuestion() {
+  void _ansewerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
   }
 
+  static const _questionList = [
+    {
+      'questionText': 'What\'s your favourate color ?',
+      'answers': ['Blue', 'Red', 'Yellow', 'Green'],
+    },
+    {
+      'questionText': 'What\'s your name ?',
+      'answers': ['Swopnil', 'Swopnil Shahi', 'Yellow', 'Green'],
+    },
+    {
+      'questionText': 'What\'s your favourate movie ?',
+      'answers': ['Blue', 'jay Nepal', 'Yellow'],
+    },
+    {
+      'questionText': 'What\'s your favourate city ?',
+      'answers': ['achham', 'kathmandu', 'Yellow', 'Green'],
+    },
+  ];
   @override
   Widget build(BuildContext context) {
-    const questionList = [
-      {
-        'questionText': 'What\'s your favourate color ?',
-        'answers': ['Blue', 'Red', 'Yellow', 'Green'],
-      },
-      {
-        'questionText': 'What\'s your name ?',
-        'answers': ['Swopnil', 'Swopnil Shahi', 'Yellow', 'Green'],
-      },
-      {
-        'questionText': 'What\'s your favourate movie ?',
-        'answers': ['Blue', 'jay Nepal', 'Yellow'],
-      },
-      {
-        'questionText': 'What\'s your favourate city ?',
-        'answers': ['achham', 'kathmandu', 'Yellow', 'Green'],
-      },
-    ];
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(
-              title: const Text(' क ख ग घ ङ च छ ज झ ञ'),
-              backgroundColor: Colors.green),
-          body: Column(children: [
-            Question(questionList[_questionIndex]['questionText'] as String),
-            // ...questionList[_questionIndex]['answers'] as List<String>.map((answer){
-            //   return Answer(ansewerQuestion,answer);
-            // }).toList(),
-            ...(questionList[_questionIndex]['answers']as List<String>).map((answer) {
-              return Answer(ansewerQuestion, answer);
-            }).toList()
-          ]),
-        ));
+            appBar: AppBar(
+                title: const Text(' क ख ग घ ङ च छ ज झ ञ'),
+                backgroundColor: Colors.green),
+            body: _questionIndex < _questionList.length
+                ? Quiz(
+                    questionList: _questionList,
+                    questionIndex: _questionIndex,
+                    ansewerQuestion: _ansewerQuestion)
+                : Result()));
   }
 }
---
+++ first_app/lib/quiz.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questionList;
+  final int questionIndex;
+  final VoidCallback ansewerQuestion;
+  Quiz(
+      {required this.questionList,
+      required this.questionIndex,
+      required this.ansewerQuestion});
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Question(questionList[questionIndex]['questionText'] as String),
+      // ...questionList[_questionIndex]['answers'] as List<String>.map((answer){
+      //   return Answer(ansewerQuestion,answer);
+      // }).toList(),
+      ...(questionList[questionIndex]['answers'] as List<String>).map((answer) {
+        return Answer(ansewerQuestion, answer);
+      }).toList()
+    ]);
+  }
+}
